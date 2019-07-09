9c0b2926273f00f1883dcd29ff88b46a11e12be8
lyy19910615 <986837786@qq.com>
2019-07-09 15:01:09 +0800

    add camera

---
+++ Scripts/teset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class teset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<int> listInt = new List<int>();
+        listInt.Add(5);
+        listInt.Add(8);
+        listInt.Add(1);
+        listInt.Add(2);
+        listInt.Add(10);
+        listInt.Add(7);
+        listInt.Add(6);
+        listInt = listInt.OrderBy(item => item).ToList();
+        for(int i = 0; i < listInt.Count; ++ i)
+        {
+            UnityEngine.Debug.Log(listInt[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
